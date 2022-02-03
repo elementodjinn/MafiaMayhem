4e3cf922f2c27bcdffb3f4604531a5e12120f6e5
Varij Jhaveri <varij.jhaveri@gmail.com>
2022-02-03 21:03:02 +0000

    varij-test Hello World

---
+++ MafiaMayhem/Assets/HelloWorld.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    int i;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
