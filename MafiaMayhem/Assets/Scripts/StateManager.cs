bfe761c55f783ef9b683853ec2ba4cbd3711ca68
Qrow99 <bkeyro99@gmail.com>
2022-02-16 14:45:09 -0800

    Fixed issues with state manager

--- MafiaMayhem/Assets/Scripts/StateManager.cs
+++ MafiaMayhem/Assets/Scripts/StateManager.cs
@@ -13,46 +13,64 @@ public class StateManager : MonoBehaviour
 
     [SerializeField] private GameObject handDisplay;
     [SerializeField] private GameObject PlayedCards;
+    [SerializeField] private GameObject handButtons;
 
     [SerializeField] private HandManager Player1Hand;
     [SerializeField] private HandManager Player2Hand;
 
-    private int turnCount = 0;
-
+    private int turnCount = 1;
+    private bool firstTurn = true;
     private void Start()
     {
+        //start on player 1's turn
         thisText.text = "Player 1, pick a card. Player 2, eyes off the screen!";
         backdrop.enabled = true;
         nextButton.enabled = true;
     }
-    public void determineCurrentPhase()
+    public void determineCurrentPhase() //more controls the screens between turns if that makes sense
     {
-        turnCount++;
         Debug.Log(turnCount);
-        if (turnCount == 1)
+        if (turnCount == 1) //player 1's turn
         {
             PlayedCards.SetActive(false);
             handDisplay.SetActive(true);
             Player1Hand.showHand();
             thisText.text = "Player 1, pick a card. Player 2, eyes off the screen!";
         }
-        else if(turnCount == 2)
+        else if(turnCount == 2) //player 2's turn
         {
             Player2Hand.showHand();
             thisText.text = "Player 2, pick a card. Player 1, no peeking!";
         }
-        else if(turnCount == 3)
+        else if(turnCount == 3) //sets up pre flip screen
         {
-            PlayedCards.SetActive(true);
-            handDisplay.SetActive(false);
             thisText.text = "Ready? FIGHT!";
             buttonText.text = "Flip Cards";
             turnCount = 0;
         }
     }
 
+    public void Flip() //flip played cards and compare them
+    {
+        if(turnCount == 0) //when turncount = 0, we can compare cards
+        {
+            PlayedCards.SetActive(true);
+            handDisplay.SetActive(false);
+            handButtons.SetActive(false);
+        }
+        else
+        {
+            handButtons.SetActive(true);
+        }
+    }
+
     public int currentplayerTurn()
     {
         return turnCount;
+    }
+
+    public void incrementTurn()
+    {
+        turnCount++;
     }
 }
